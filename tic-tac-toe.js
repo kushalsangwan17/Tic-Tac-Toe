37af1ea662ac8b7383816a9e4767f4a3c58c1c60
kushalsangwan17 <53648987+kushalsangwan17@users.noreply.github.com>
2020-08-05 21:14:58 +0530

    Update tic-tac-toe.js

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -7,8 +7,57 @@ function myfunction(item){
 		else
 			item.textContent="X";
 
-		
+		if(document.getElementById("td1").textContent!="" && document.getElementById("td1").textContent==document.getElementById("td2").textContent && document.getElementById("td2").textContent==document.getElementById("td3").textContent)
+		{
+			alert("game ends");
+			location.reload();
+		}
+
+		if(document.getElementById("td4").textContent!="" && document.getElementById("td4").textContent==document.getElementById("td5").textContent && document.getElementById("td5").textContent==document.getElementById("td6").textContent)
+		{
+			alert("game ends");
+			location.reload();
+		}
+
+		if(document.getElementById("td7").textContent!="" && document.getElementById("td7").textContent==document.getElementById("td8").textContent && document.getElementById("td8").textContent==document.getElementById("td9").textContent)
+		{
+			alert("game ends");
+			location.reload();
+		}
+
+		if(document.getElementById("td1").textContent!="" && document.getElementById("td1").textContent==document.getElementById("td4").textContent && document.getElementById("td4").textContent==document.getElementById("td7").textContent)
+		{
+			alert("game ends");
+			location.reload();
+		}
+
+		if(document.getElementById("td2").textContent!="" && document.getElementById("td2").textContent==document.getElementById("td5").textContent && document.getElementById("td5").textContent==document.getElementById("td8").textContent)
+		{
+			alert("game ends");
+			location.reload();
+		}
+
+		if(document.getElementById("td3").textContent!="" && document.getElementById("td3").textContent==document.getElementById("td6").textContent && document.getElementById("td6").textContent==document.getElementById("td9").textContent)
+		{
+			alert("game ends");
+			location.reload();
+		}
+
+		if(document.getElementById("td1").textContent!="" && document.getElementById("td1").textContent==document.getElementById("td5").textContent && document.getElementById("td5").textContent==document.getElementById("td9").textContent)
+		{
+			alert("game ends");
+			location.reload();
+		}
+
+		if(document.getElementById("td3").textContent!="" && document.getElementById("td3").textContent==document.getElementById("td5").textContent && document.getElementById("td5").textContent==document.getElementById("td7").textContent)
+		{
+			alert("game ends");
+			location.reload();
+		}
+
 	})
+
+
 }
 
 
